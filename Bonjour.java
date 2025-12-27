85fe0553b0d2ba18e5a59ccc118a07e912f4f88a
Abdelaaly-bouzaroual <abdelaalybouzaroual@gmail.com>
2025-12-27 17:46:10 +0100

    Ajout de code sale pour tester Sonar

--- Bonjour.java
+++ Bonjour.java
@@ -1,1 +1,11 @@
-public class Bonjour { public static void main(String[] args) { System.out.println("Elhamdolla, this is my first step on DevOps");  } }
+public class Bonjour {
+    public static void main(String[] args) {
+        // Code Smell 1 : Variable inutile
+        String variableInutile = "Je ne sers a rien";
+        
+        // Security Hotspot : Mot de passe en dur
+        String password = "admin"; 
+
+        System.out.println("Elhamdollah, this is my first step on DevOps");
+    }
+}
